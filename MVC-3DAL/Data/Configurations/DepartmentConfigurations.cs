407358fd759e9238ffc748d2ee0261597f6b7695
Mariam Mahmoud <105247169+MariamMahmoud2909@users.noreply.github.com>
2024-03-21 15:31:10 +0200

    Add project files.

---
+++ MVC-3DAL/Data/Configurations/DepartmentConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using MVC_3DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MVC_3DAL.Data.Configurations
+{
+    internal class DepartmentConfigurations : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id);
+            builder.Property(D => D.Name).IsRequired().HasColumnType("varchar").HasMaxLength(50);
+            builder.Property(D => D.Code).IsRequired().HasColumnType("varchar").HasMaxLength(50);
+        }
+    }
+}
