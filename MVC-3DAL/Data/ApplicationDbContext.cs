f853ca6af4bf1a8abbbae65e2309dabe7983d3c6
Mariam Mahmoud <105247169+MariamMahmoud2909@users.noreply.github.com>
2024-03-22 22:12:24 +0200

    test

---
+++ MVC-3DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_3DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_3DAL.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<Department> Departments { get; set; }
+
+    }
+}
---
+++ MVC-3PL/Controllers/DepartmentController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_3DAL.Models;
+using MVC_3BLL.Interfaces;
+using MVC_3BLL.Repositories;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace MVC_3PL.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public DepartmentController(IDepartmentRepository depatmentRepository)
+        {
+            _departmentRepository = depatmentRepository;
+        }
+        public IActionResult Index()
+        {
+            var departments = _departmentRepository.GetAll();
+            return View(departments);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Department department)
+        {
+            if (ModelState.IsValid) // server side validation
+            {
+                var count = _departmentRepository.Add(department);
+                if (count > 0)
+                    return RedirectToAction(nameof(Index));
+            }
+            return View(department);
+        }
+    }
+}
