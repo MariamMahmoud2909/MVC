5b1bf3b7c32860814851f7226269000c4968e8d2
Mariam Mahmoud <105247169+MariamMahmoud2909@users.noreply.github.com>
2024-03-23 20:05:30 +0200

    Employee Repository - BLL

---
+++ MVC-3BLL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using MVC_3DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_3BLL.Interfaces
+{
+	public interface IEmployeeRepository
+	{
+		IEnumerable<Employee> GetAll();
+		Employee Get(int id);
+		int Add(Employee entity);
+		int Update(Employee entity);
+		int Delete(Employee entity);
+	}
+}
---
+++ MVC-3BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,50 @@
+﻿using MVC_3BLL.Interfaces;
+using MVC_3DAL.Data;
+using MVC_3DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_3BLL.Repositories
+{
+	internal class EmployeeRepository : IEmployeeRepository
+	{
+		private readonly ApplicationDbContext _dbContext;
+		public EmployeeRepository(ApplicationDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+		public int Add(Employee entity)
+		{
+			_dbContext.Add(entity);
+			return _dbContext.SaveChanges();
+		}
+
+		public int Delete(Employee entity)
+		{
+			_dbContext.Remove(entity);
+			return _dbContext.SaveChanges();
+		}
+
+		public Employee Get(int id)
+		{
+			///var Employee = _dbContext.Employees.Local.Where(D => D.Id == id).FirstOrDefault();
+			///if(Employee == null)
+			///    Employee = _dbContext.Employees.Where(D => D.Id == id).FirstOrDefault();
+			///return Employee;
+			///
+			return _dbContext.Employees.Find(id);
+		}
+
+		public IEnumerable<Employee> GetAll()
+			=> _dbContext.Employees.ToList();
+
+		public int Update(Employee entity)
+		{
+			_dbContext.Update(entity);
+			return _dbContext.SaveChanges();
+		}
+	}
+}
